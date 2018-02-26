25a9f37d2dde041e6494ebe503c8929a9b241a27
juniorrochas97 <juniorrochas87@gmail.com>
2018-02-26 16:34:47 -0400

    Update conecta.php

--- config/conecta.php
+++ config/conecta.php
@@ -4,7 +4,7 @@ $conn = 'mysql:host=localhost:3307;dbname=curso_php';
 
 try {
 //     conecta ao banco,'login','senha'
-	$db = new PDO($conn,'root','Solange@19972');
+	$db = new PDO($conn,'user_curso','Suasenha');
 
 	$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
